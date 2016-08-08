ee5c92485a234bfa7fd139edd2a31f6d6dde0d38
Eric Lagergren <ericscottlagergren@gmail.com>
2016-08-08 13:33:31 -0700

    add NewPopulated

--- protobuf.go
+++ protobuf.go
@@ -10,3 +10,15 @@ func (u UUID) MarshalTo(data []byte) (int, error) {
 	copy(data, u[:])
 	return 16, nil
 }
+
+type randy interface {
+	Intn(n int) int
+}
+
+func NewPopulatedUUID(r randy) *UUID {
+	var u UUID
+	for i := 0; i < len(u); i++ {
+		u[i] = byte(r.Intn(255))
+	}
+	return &u
+}
