701fea438b2edd09f9fe934d34632e40b0a80a8b
Eric Lagergren <ericscottlagergren@gmail.com>
2016-08-08 13:40:40 -0700

    add more protobuf methods

--- protobuf.go
+++ protobuf.go
@@ -2,13 +2,34 @@
 
 package uuid
 
+import "bytes"
+
 func (u UUID) Size() int {
 	return len(u)
 }
 
 func (u UUID) MarshalTo(data []byte) (int, error) {
-	copy(data, u[:])
-	return 16, nil
+	return copy(data, u[:]), nil
+}
+
+func (u *UUID) Unmarshal(data []byte) error {
+	return u.UnmarshalBinary(data)
+}
+
+func (u UUID) Marshal() ([]byte, error) {
+	return u.MarshalBinary()
+}
+
+func (u UUID) MarshalJSON() ([]byte, error) {
+	return []byte(`"` + u.MarshalText() + `"`)
+}
+
+func (u *UUID) UnmarshalJSON(data []byte) error {
+	return u.UnmarshalText(data)
+}
+
+func (u UUID) Compare(u2 UUID) int {
+	return bytes.Compare(u[:], u2[:])
 }
 
 type randy interface {
